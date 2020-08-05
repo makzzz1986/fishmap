07c565070ba8e8881eb738b9cc5c333e5187fb02
maxim.pisarenko <maxim.pisarenko@olx.com>
2020-08-05 09:17:08 +0100

    Intersection 1

--- draw.py
+++ draw.py
@@ -1,4 +1,5 @@
 import geopandas
+import pandas
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString
 import scipy.special as sc
@@ -33,10 +34,10 @@ def wave_line(start_point, angle, bbox):
     end_point_x = xmax
     end_point_y = ((xmax - start_point[0]) * sc.tandg(angle)) + start_point[1]
     if end_point_y > ymax:
-        print('Too big!')
+        # print('Too big!')
         end_point_y = ymax
         end_point_x = ((ymax - start_point[1]) * sc.cotdg(angle)) + start_point[0]
-    print(start_point, (end_point_x, end_point_y))
+    # print(start_point, (end_point_x, end_point_y))
     return [start_point, (end_point_x, end_point_y)]
 
 bbox = (-9.48859, 38.71225, -9.48369, 38.70596)
@@ -61,19 +62,30 @@ for _, n, r in coast.itertuples():
 real_bbox = (xmin, ymin, xmax, ymax)
 
 wave_angle = 30
+precision = 30
 
-ytenths = (ymax - ymin) / 10
-xtenths = (xmax - xmin) / 10
-for ten in range(10):
-    # coast.loc[len(coast), 'geometry'] = LineString([(xmin, ymin + (ytenths * ten)), (xmax, ymin + (ytenths * ten))])
-    # coast.loc[len(coast), 'geometry'] = LineString([(xmin, ymin + (ytenths * ten)), (xmax, ymin + height(wave_angle, xmax-xmin, ymax-ymin) + (ytenths * ten))])
-    # coast.loc[len(coast), 'geometry'] = LineString([(xmin + (xtenths * ten), ymin), (xmax + (xtenths * ten), ymin + height(wave_angle, xmax-xmin))])
-    coast.loc[len(coast), 'geometry'] = LineString(wave_line((xmin, ymin + (ytenths * ten)), wave_angle, real_bbox))
-    coast.loc[len(coast), 'geometry'] = LineString(wave_line((xmin + (xtenths * ten), ymin), wave_angle, real_bbox))
-    # print(ymin + (ytenths * ten))
+ytenths = (ymax - ymin) / precision
+xtenths = (xmax - xmin) / precision
+
+# draw wave lines
+waves = geopandas.GeoDataFrame([], columns=['geometry'], crs="EPSG:4326")
+for ten in range(precision):
+    waves.loc[len(waves), 'geometry'] = LineString(wave_line((xmin, ymin + (ytenths * ten)), wave_angle, real_bbox))
+    waves.loc[len(waves), 'geometry'] = LineString(wave_line((xmin + (xtenths * ten), ymin), wave_angle, real_bbox))
+
+# for ten in range(precision):
+#     coast.loc[len(coast), 'geometry'] = LineString(wave_line((xmin, ymin + (ytenths * ten)), wave_angle, real_bbox))
+#     coast.loc[len(coast), 'geometry'] = LineString(wave_line((xmin + (xtenths * ten), ymin), wave_angle, real_bbox))
 
 
-print('bbox', bbox, 'real_bbox: ', (xmin, ymin), (xmax, ymax))
+intersection = geopandas.tools.sjoin(coast, waves)
+# print(type(intersection))
+
+
+combined = geopandas.GeoDataFrame(pandas.concat([coast, intersection], ignore_index=True))
+coast.loc[len(coast), 'geometry'] = intersection
+
+# print('bbox', bbox, 'real_bbox: ', (xmin, ymin), (xmax, ymax))
 
 # add bboxes to plot
 coast.loc[len(coast), 'geometry'] = LineString([(bbox[0], bbox[1]), (bbox[2], bbox[1])])
@@ -87,7 +99,8 @@ coast.loc[len(coast), 'geometry'] = LineString([(real_bbox[2], real_bbox[3]), (r
 coast.loc[len(coast), 'geometry'] = LineString([(real_bbox[0], real_bbox[3]), (real_bbox[0], real_bbox[1])])
 
 
-print(coast)
+# print(coast)
+# print(waves)
 
 coast.plot()
 plt.show()
